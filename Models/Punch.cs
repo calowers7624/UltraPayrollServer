a783a657c6e2bc58a56bfac844ae6a1e869d306a
calowers7624 <47893911+calowers7624@users.noreply.github.com>
2022-11-25 20:01:10 -0500

    test2

---
+++ Models/Punch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltraPayrollServer.Models
+{
+    public class Punch
+    {
+        public int ID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public DateTime PunchTime { get; set; }
+        //true if is a punch-in, false if punch-out
+        public bool InOut { get; set; }
+    }
+}
